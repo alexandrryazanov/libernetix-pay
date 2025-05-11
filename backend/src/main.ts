a855f5ea944ef340d7fec8a146e2ce6e3f704fed
ryazanov <alexandrscooterryazanov@gmail.com>
2025-05-11 15:54:05 +0500

    feat(security): helmet + rate-limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,21 @@ import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { raw } from 'express';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
   app.use('/webhooks/libernetix', raw({ type: '*/*' })); // to get body buffer to verify
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      limit: 100,
+      message: 'Too many requests from this IP, please try again later.',
+    }),
+  );
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
